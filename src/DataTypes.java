bed9598115e8df3975c1a73b017c165faed9fc61
Ariffudholi <reginamudawamah@gmail.com>
2021-06-05 21:09:42 +0800

    deklarasi variabel dan tipe data

---
+++ src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args){
+
+        String namaDepan = "Regina ";
+        String namaBelakang = "Mudawamah";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.389764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+    }
+}
