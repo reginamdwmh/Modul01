4199e763d02bf71ae4b10f4f6b1ac65d56301d5c
Ariffudholi <reginamudawamah@gmail.com>
2021-06-05 21:58:20 +0800

    Operasi Tipe String

--- Modul01_IDEA/src/Main.java
+++ Modul01_IDEA/src/Main.java
@@ -1,20 +1,39 @@
+import java.util.Locale;
+
 public class Main {
-    public static void main (String[] args ){
+    public static void main (String[] args ) {
         String namaDepan = "Regina";
         String namaBelakang = "Mudawamah";
-        int usia = 18 ;
+        int usia = 18;
         int targetTahunKuliah = 4;
         double ipk = 3.89764512;
         char nilaiAbjad = 'A';
-        boolean cantik = true ;
+        boolean cantik = true;
 
         System.out.println(namaDepan + namaBelakang);
 
 
-
-
-        char [] uniskaChar = {'U','N','I','S','K','A'};
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("Reg", "Nan"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
+
+
     }
 }
