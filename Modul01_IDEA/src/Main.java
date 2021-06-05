4e3cd3969a9e49b419735734835d274758c296e4
Ariffudholi <reginamudawamah@gmail.com>
2021-06-05 21:45:41 +0800

    Project IDEA

---
+++ Modul01_IDEA/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args ){
+        String namaDepan = "Regina";
+        String namaBelakang = "Mudawamah";
+        int usia = 18 ;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true ;
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
